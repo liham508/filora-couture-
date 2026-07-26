a30f56a8ab02ad8d1331e5f1bf4535884363983a
liham508 <nounamina153@gmail.com>
2026-07-26 03:38:37 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -397,6 +397,8 @@ onChange={(e) => setAdresse(e.target.value)}
                 <div className="flex gap-3">
                   <input
                     placeholder="Ville"
+                    value={ville}
+onChange={(e) => setVille(e.target.value)}
                     className="border border-[#4A3F41]/20 rounded-xl px-4 py-2.5 text-sm bg-white/60 flex-1"
                   />
                   <input
